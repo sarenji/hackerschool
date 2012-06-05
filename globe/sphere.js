40a1c6d1e879d7f0794c55ddf95fd06b99a4eb34
David Peter <david.a.peter@gmail.com>
2012-06-05 14:43:37 -0400

    Lower intensity

--- globe/sphere.js
+++ globe/sphere.js
@@ -55,7 +55,7 @@ var Shaders = {
       'varying vec2 vUv;',
       'void main() {',
         'vec3 diffuse = texture2D( texture, vUv ).xyz;',
-        'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
+        'float intensity = 1.0 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
         'vec3 atmosphere = vec3( 0.47, 0.84, 1.0 ) * pow( intensity, 4.0 );',
         'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
       '}'
